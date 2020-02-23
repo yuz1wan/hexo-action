9d1dba50069465925dcf66d39845239c34a5c2dd
deppwang <deppwang@163.com>
2020-02-23 20:40:59 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,7 +15,6 @@ git config --global user.email "$INPUT_USER_EMAIL"
 # install hexo env
 npm install hexo-cli -g
 npm install hexo-deployer-git --save
-npm install hexo --save
 npm install hexo-algolia --save
 
 # deployment
