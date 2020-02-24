c9c7bc5a927f71baf6ae1d9cce349d962bc9f125
deppwang <deppwang@163.com>
2020-02-24 23:15:24 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -23,7 +23,6 @@ hexo g -d
 # algolia
 export HEXO_ALGOLIA_INDEXING_KEY=d3e6a74afaebeb6b9c3a26eee410f08e
 echo $HEXO_ALGOLIA_INDEXING_KEY
-cd ./node_modules/.bin/
 hexo algolia
 
 echo ::set-output name=notify::"Deploy complate."
